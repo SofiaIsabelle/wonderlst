ed8a203460e8a11d325f6123e8ad10289dce3bb1
Sofia Isabelle <sofia.i.rahimi@gmail.com>
2019-01-12 23:15:19 -0600

    created new template and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :tasks
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about' => 'pages#about'
   #when you see about after the root directory , you go into the pages controller in order to access the about action
-
+  
 end
