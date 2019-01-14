25cbea95dfd51482bd3ac69b7768bf38b265e6ed
Sofia Isabelle <sofia.i.rahimi@gmail.com>
2019-01-14 16:26:53 -0600

    created user-tasks association using devise & updated controller

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord #inherits from Application Record
 validates :content, presence: true, length: {minimum: 5, maximum: 300}
+
+belongs_to :user
 end	
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_13_021330) do
+ActiveRecord::Schema.define(version: 2019_01_14_221027) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
