7f580176d1da67a8b4acabe3dd2a299d7186de2a
Sofia Isabelle <sofia.i.rahimi@gmail.com>
2019-01-16 12:19:02 -0600

    updated form styling + signin logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 	#child of application controller
 	#inherits code from application controller and uses it's own code
   def home
+  	if current_user
+  		redirect_to tasks_path
+  	end	
   end
 
   def about 
