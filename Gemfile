c5816e4c324f236b08fabf2dfc764c5c5812da08
Sofia Isabelle <sofia.i.rahimi@gmail.com>
2019-01-16 12:59:11 -0600

    added gem 'auto_html', '~>1.6.4' to post links ,videos and images as tasks

--- Gemfile
+++ Gemfile
@@ -65,5 +65,9 @@ gem "twitter-bootstrap-rails"
 gem "font-awesome-rails"
 gem 'simple_form'
 gem 'devise'
+gem 'auto_html', '~>1.6.4'
+
+
+
 
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,15 @@ class Task < ApplicationRecord #inherits from Application Record
 validates :content, presence: true, length: {minimum: 5, maximum: 300}
 
 belongs_to :user
+
+auto_html_for :content do 
+
+html_escape
+image
+youtube(width: '100%', height: 250)
+link target: '_blank', rel: 'nofollow'
+simple_format
+
+end
 end	
 
